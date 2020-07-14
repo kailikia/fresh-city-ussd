c9d0b97df5c931aea7f9933ebbdaa1d3fcdceac6
tech-camp <edwinkailikia@gmail.com>
2020-07-14 15:44:37 +0300

    interface one

--- app/config.py
+++ app/config.py
@@ -6,6 +6,10 @@ import random
 import string
 import re
 
+# DB_URL = 'postgresql://postgres:123456@127.0.0.1:5432/freshcity_ussd'
+
+DB_URL = 'postgresql://myuser:mypass@172.17.0.1:5432/freshcity_ussd'
+
 app = Flask(__name__)
 app.secret_key = 'Sc$4677*&E%9%d3DW2@#E465*&rc4e*3'
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
@@ -13,10 +17,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] ='some-secret-string'
 
 db = SQLAlchemy(app)
-
-# DB_URL = 'postgresql://postgres:123456@127.0.0.1:5432/freshcity_ussd'
-
-DB_URL = 'postgresql://myuser:mypass@172.17.0.1:5432/freshcity_ussd'
 
 
 
