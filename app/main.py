c8a05b8e5ba23e7159e81e0ddbf0dc4b6ef8a0a6
tech-camp <edwinkailikia@gmail.com>
2020-07-14 16:01:19 +0300

    interface one 5

--- app/main.py
+++ app/main.py
@@ -77,7 +77,7 @@ def ussdSession():
         menu = manyMenu
 
     elif  len(textArray) == 4:
-        payment = USSDModel(sessionID=sessionId,phoneNumber=phoneNumber,name=textArray[0],county=textArray[1],products=textArray[2],farm=textArray[3])
+        payment = USSDModel(sessionID=sessionId,phoneNumber=phoneNumber,name=textArray[0],county=textArray[1],products=textArray[2],ready=textArray[3])
         payment.create_record()
         menu = successMenu
 
